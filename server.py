be8342617b5ba22bde52d0b8c6d94ac7c3a216ae
Ramon Felipe Pereira <ramoneesfp@gmail.com>
2025-05-26 14:29:56 +0800

    start to change to MongoDB and remove graph

--- server.py
+++ server.py
@@ -1,38 +1,24 @@
 from flask import Flask, render_template,request
-import mysql.connector
+from pymongo import MongoClient
+from bson import ObjectId
 
-mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="Ramon1306!", #AWSDB -> Ramon130694!
-    database="price_trend"
-)
+client = MongoClient("mongodb://localhost:27017")
+collection_link = client["budget"]["link_budget"]
+collection_info = client["budget"]["info_budget"]
+collection_id = client["budget"]["id_budget"]
+col_id = collection_id.find({})
+col_id = list(col_id)
+print(col_id)
 
-mycursor = mydb.cursor()
+myresult = col_id
 
-mycursor.execute("SELECT * FROM price_trend.test_gru")
-
-myresult = mycursor.fetchall()
-
-#plot_date_arr = []
-#plot_min_prince = []
-# for y in myresult:
-#     plot_date_arr.append(y[4].strftime("%y-%m-%d"))
-#     plot_min_prince.append(y[5])
-#     #print(y)
-#
-# print(plot_date_arr)
-# print(plot_min_prince)
-
-plot_min_prince = ['248', '249', '237', '197', '225', '175', '174', '187', '232', '174', '11', '1']
-plot_date_arr = plot_min_prince
-headings = ["Id", "Cod_dep", "Cod_arr", "Date_Search", "Date_arr", "min_price", "med_price", "obs"]
+headings = ["Id", "cpf", "phone_number", "name", "surname", "email", "date_request", "obs"]
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", headings=headings, data=myresult, plot_date_arr=plot_date_arr, plot_min_prince=plot_min_prince)
+    return render_template("index.html", headings=headings, data=myresult)
 
 # SOLUTION to Challenge:
 @app.route("/form-entry", methods=["POST"])
@@ -40,7 +26,7 @@ def receive_data():
     name = request.form["demo-name"]
     email = request.form["demo-email"]
     print(f'name:{name} and email:{email}')
-    return render_template("index.html", headings=headings, data=myresult, plot_date_arr=plot_date_arr, plot_min_prince=plot_min_prince)
+    return render_template("index.html", headings=headings, data=myresult)
 
 if __name__ == "__main__":
     #app.run(debug=True, host= '192.168.0.219')
