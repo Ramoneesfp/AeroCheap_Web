1ef7113f59f21002e1b0d3565036256130fbf97e
Ramon Felipe Pereira <ramoneesfp@gmail.com>
2025-05-27 09:36:19 +0800

    send a dynamic listbox OK

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ from datetime import datetime
 #myresult = col_id
 
 #headings = ["Id", "cpf", "phone_number", "name", "surname", "email", "date_request", "obs"]
+headings = ["", "Com retorno", "Só ida"]
 
 def insert_id_info(name, surname, cpf, fone, email, cod_dep, cod_arr, ticket_type, adults, date_dep, date_ret):
     client = MongoClient("mongodb://localhost:27017")
@@ -56,7 +57,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", headings=headings)
 
 # SOLUTION to Challenge:
 @app.route("/form-entry", methods=["POST"])
@@ -80,93 +81,10 @@ def receive_data():
 
     #informar pagina que inseriu
 
-    return render_template("index.html")
+    return render_template("index.html", headings=headings)
 
 if __name__ == "__main__":
-    app.run(debug=True, host= '192.168.20.5')
+    app.run(debug=True, host= '172.20.63.108')
     #app.run(debug=True)
-
-    #INSERT VALUE INTO MONGODB s
-
-    # from pymongo import MongoClient
-    # client = MongoClient("mongodb://localhost:27017")
-    # collection_id = client["budget"]["id_budget"]
-    # col_id = collection_id.find({})
-    # col_id = list(col_id)
-    # print(col_id)
-    #
-    # collection_info = client["budget"]["info_budget"]
-    # col_info = collection_info.find({})
-    # col_info = list(col_info)
-    # print(col_info)
-    #
-    # collection_link = client["budget"]["link_budget"]
-    # col_link = collection_link.find({})
-    # col_link = list(col_link)
-    # print(col_link)
-    #
-
-    # aggregate_info = {
-    #     "cod_dep": "CWB",
-    #     "cod_arr": "GRU",
-    #     "ticket_type": 2,
-    #     "adults": 1,
-    #     "children": 0,
-    #     "infants_in_seat": 0,
-    #     "infants_on_lap": 0,
-    #     "seating_class": 0,
-    #     "date_dep": "2025/07/08",
-    #     "date_ret": "2025/08/08",
-    #     "price_searched": 500,
-    #     "state": "INI",
-    #     "number_budgets": 10,
-    #     "id_budget": {
-    #         "$oid": result.inserted_id
-    #     },
-    #     "obs": ""
-    # }
-    #
-    # aggregate_link = {
-    #     "price": [
-    #         1,
-    #         2
-    #     ],
-    #     "localization": [
-    #         "o1",
-    #         "o2"
-    #     ],
-    #     "client_summarize": [
-    #         "oi1",
-    #         "oi2"
-    #     ],
-    #     "complete_info": [
-    #         "alot1",
-    #         "alot2"
-    #     ],
-    #     "baggage": [
-    #         "",
-    #         ""
-    #     ],
-    #     "link": [
-    #         "www1",
-    #         "www2"
-    #     ],
-    #     "date_search": [
-    #         "2025/05/20",
-    #         "2025/05/20"
-    #     ],
-    #     "id_budget": {
-    #         "$oid": result.inserted_id
-    #     },
-    #     "obs": [
-    #         "",
-    #         ""
-    #     ]
-    # }
-
-    # result = collection_info.insert_one(aggregate_info)
-    # print(f"Inserted document ID: {result.inserted_id}")
-    # result = collection_link.insert_one(aggregate_link)
-    # print(f"Inserted document ID: {result.inserted_id}")
 
 
